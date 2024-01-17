c99044d73a47b936b28f724ac3e06e45289b7fb9
Angel Motta <angel.motta@utec.edu.pe>
2024-01-17 13:00:32 -0500

    feat: group anagrams

---
+++ python/arrays-strings/groupAnagram.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/group-anagrams/description/
+from typing import List
+
+# Time O(n * m) where `n` is list's length strs and `m` is the average length of each word in the list strs
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mapGroupAnagrams = {}
+        for str in strs:
+            counterWord = [0] * 26 # a-z chars
+            for c in str:
+                idx = ord(c) - ord('a')
+                counterWord[idx] += 1
+            keyCounterWord = tuple(counterWord) # making list unmutable (for hashing)
+            if keyCounterWord in mapGroupAnagrams:
+                mapGroupAnagrams[keyCounterWord].append(str)
+            else:
+                mapGroupAnagrams[keyCounterWord] = [str]
+        return list(mapGroupAnagrams.values())
+
+
+solver = Solution()
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+out = [["bat"],["nat","tan"],["ate","eat","tea"]]
+ans = solver.groupAnagrams(strs)
+print(ans)
